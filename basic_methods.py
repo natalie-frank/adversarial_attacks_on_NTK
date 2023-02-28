a5078c421f700331d726efe774ece75d80c8c5dc
Natalie Frank <nf1066@nyu.edu>
2023-02-28 14:34:19 -0500

    initial commit

---
+++ basic_methods.py
@@ -0,0 +1,43 @@
+import os
+import torch
+from matplotlib import pyplot
+import platform
+
+
+#sets current working directory
+def set_cwd():
+    if platform.platform() == "Windows-10-10.0.19041-SP0":
+        cwd=os.path.join("C:\\Users","Natalie Frank","Documents","PNNL code","bitbucket")
+    else:
+        cwd="todo"#todo
+    os.chdir(cwd)
+
+#checkes if the directory dir_name exists, and creates it if it doesn't
+#returns true if the directory was created and false otherwise
+def exist_and_create_dir(dir_name):
+    ext=os.path.isdir(dir_name)
+    if not ext:
+        os.mkdir(dir_name)
+    return ~ext
+
+
+
+#net: a neural net
+#xs: a tensor of data points, the first dimension is assumed to be the batch dimension
+#class_names: the names of each class corresponding to an output neuron, can be none
+
+#if class_names is None, returns a tensor of the index of the maximum output neuron at each data point in xs.
+#otherwise resturns a list of the predicted classes of each data point in xs as specified by class_names 
+def predict_class(net, xs, class_names=None):
+    ys=net(xs)
+    inds=torch.max(ys,1)[1]
+    if class_names is None:
+        return inds
+    else:
+        lst=[class_names[i.item()] for i in inds]
+        return lst
+
+def plot_grayscale(x):
+    pyplot.imshow(x, cmap=pyplot.get_cmap('gray'))
+    pyplot.show()
+
